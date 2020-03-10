d1dfa79cc1cff598180eec6a716464caddbde7f0
evelynjulia <evewicksteed@gmail.com>
2020-03-10 11:14:46 -0700

    renaming col names

--- Scripts/clean_data.R
+++ Scripts/clean_data.R
@@ -25,13 +25,25 @@ opt <- docopt(doc)
 
 
 main <- function(data_dir, infilename, outfilename){
+  # read in data
   data <- readr::read_csv(here::here(data_dir, infilename))
+  
   #convert Missing Values (tagged with -200 value) to NA
   data[data == -200] = NA 
   # Convert numeric columns to 'double' type
   data[3:15] <- sapply(data[3:15], as.double)
   data = data %>% 
     mutate(Date_Time = ymd_hms(paste(data$Date, data$Time)))
+  
+  # change col names:
+  newnames <- c("Date", "Time", "CO", "Tin_oxide", "Hydro_carbons", "Benzene", 
+                "Titania", "NOx", "Tungsten_oxide_NOx", "NO2", "Tungsten_oxide_NO2", 
+                "Indium_oxide", "Temp", "RH", "AH")
+  
+  for (i in 1:ncol(data)){
+    names(data)[i]=newnames[i]
+  }
+  
   
   #save as csv
   readr::write_csv(data, here::here(data_dir, outfilename))
