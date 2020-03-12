8a5575be62dde9f9bfff9b23f98e06fd1304339a
Anh-NguyetLe <anh.le0804@gmail.com>
2020-03-11 19:54:34 -0700

    Plot of coefficients

--- Scripts/LinearRegression.R
+++ Scripts/LinearRegression.R
@@ -13,6 +13,7 @@ Usage: LinearRegression.R --path=<path> --datafilename=<datafilename>
 " -> doc
 
 library(broom)
+library(dotwhisker)
 library(here)
 library(tidyverse)
 library(DT)
@@ -29,36 +30,58 @@ opt <- docopt(doc)
 
 main <- function(path, datafilename){
   
-  #read data
+  ##### READ DATA
   data <- readr::read_csv(here::here(glue::glue(path, datafilename)))
   
-  #Aggregate Daily Average
+  
+  ##### Aggregate Daily Average
   airq_daily = airq %>%
     group_by(Date) %>%
     summarise_all(funs(mean), na.rm = TRUE)
   
-  ##### REGRESSION
+  
+  ##### REGRESSIONs
+  
   #Fit benzene against temperature & humidity (relative humidity is correlated to temperature) 
-  tempbenzene = lm(`C6H6(GT)` ~ T + AH, data = airq_daily)
+  Benzene = lm(`C6H6(GT)` ~ T + AH, data = airq_daily)
   #save RDS file
-  saveRDS(tempbenzene, file = "tempbenzene.rds")
-  tidy(tempbenzene)
+  saveRDS(Benzene, file = "Benzene.rds")
+  tidy(Benzene)
   
   #Fit titania against temperature & humidity 
-  temptitania = lm(`PT08.S2(NMHC)` ~ T + AH, data = airq_daily)
+  Titania = lm(`PT08.S2(NMHC)` ~ T + AH, data = airq_daily)
   #save RDS file
-  saveRDS(temptitania, file = "temptitania.rds")
-  tidy(temptitania)
+  saveRDS(Titania, file = "Titania.rds")
+  tidy(Titania)
   
   #Fit tin oxide against temperature & humidity 
-  temptinoxide = lm(`PT08.S1(CO)` ~ T + AH, data = airq_daily)
+  Tinoxide= lm(`PT08.S1(CO)` ~ T + AH, data = airq_daily)
   #save RDS file
-  saveRDS(temptinoxide, file = "temptinoxide.rds")
-  tidy(temptinoxide)
+  saveRDS(Tinoxide, file = "Tinoxide.rds")
+  tidy(Tinoxide)
   
   
   ##### COEFFICIENTS PLOTS
   
+  threeM <- rbind(tidy(Benzene) %>% mutate(model = "Benzene"), 
+                  tidy(Titania) %>% mutate(model = "Titania"), 
+                  tidy(Tinoxide) %>% mutate(model = "Tin oxide")) %>% 
+    relabel_predictors(c("(Intercept)" = "Intercept",
+                         T = "Temperature",
+                         AH = "Absolute Humidity"))
+  
+  small_multiple(threeM) +
+    theme_bw() + 
+    xlab("") +
+    ylab("") +
+    ggtitle("Coefficient Estimates for Predicting Air Pollutants' Concentrate") +
+    theme(plot.title = element_text(size = 15, hjust = 0.5, family="serif"),
+          legend.position = "none",
+          legend.background = element_rect(colour="grey80"),
+          legend.title = element_blank(),
+          legend.key.size = unit(15, "pt"))
+  
+  ggsave("Images/CoefPlot_Group4.png", device = 'png', width = 7, height = 7, units = "in")
   
 }
 
